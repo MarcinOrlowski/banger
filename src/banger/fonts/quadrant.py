a8eab31b721784c44dc55626efe6448dc54d543f
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2025-06-22 19:13:13 +0200

    Updated quadrant font

--- src/banger/fonts/quadrant.py
+++ src/banger/fonts/quadrant.py
@@ -407,7 +407,7 @@ class QuadrantFont(BaseFont):
                     "   █",
                     "▄▀▀█",
                     "█  █",
-                    "▀▄▄▀",
+                    "▀▄▄█",
                     "",
                 ]
             },
